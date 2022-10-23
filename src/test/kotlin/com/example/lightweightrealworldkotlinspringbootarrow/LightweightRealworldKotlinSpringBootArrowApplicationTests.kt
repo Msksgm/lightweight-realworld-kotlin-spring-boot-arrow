708cdd92a0809efd117b18a6279280422c1a68d2
Msksgm <msksgm3124@gmail.com>
2022-10-23 10:56:34 +0900

    first commit

---
+++ src/test/kotlin/com/example/lightweightrealworldkotlinspringbootarrow/LightweightRealworldKotlinSpringBootArrowApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.lightweightrealworldkotlinspringbootarrow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LightweightRealworldKotlinSpringBootArrowApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
